bd872f6ddd16d92340ab7f11e3d1fba16313672b
John Cowen <jcowen@hashicorp.com>
2020-04-20 16:34:47 +0000

    Use the inherited respondForQuery

--- ui-v2/app/serializers/dc.js
+++ ui-v2/app/serializers/dc.js
@@ -2,11 +2,6 @@ import Serializer from './application';
 
 export default Serializer.extend({
   primaryKey: 'Name',
-  respondForQuery: function(respond, query) {
-    return respond(function(headers, body) {
-      return body;
-    });
-  },
   normalizePayload: function(payload, id, requestType) {
     switch (requestType) {
       case 'query':
